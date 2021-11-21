4fd76333591f6b82cece9cf0a2e73e0890fc67fc
benji77fr <b.girardpro@gmail.com>
2021-11-21 16:47:25 +0100

    Add Camera component, OrbitControls

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 import Sun from './components/Sun'
 import Lights from './components/Lights'
+import Planet from './components/Planet'
 
 import './App.css'
 
@@ -10,9 +12,11 @@ function App() {
 
   return (
     <div className="solarSystem">
-      <Canvas>
+      <Canvas camera={{ position: [0, 20, 25], fov: 45}}>
         <Sun />
+        <Planet />
         <Lights />
+        <OrbitControls />
       </Canvas>
     </div>
   )
