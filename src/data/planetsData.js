b650d4f7b1dbf56e33fb234ef290e13ec3dbaaa3
Benjamin <b.girardpro@gmail.com>
2021-11-22 15:34:42 +0100

    Create data object for planets

---
+++ src/data/planetsData.js
@@ -0,0 +1,56 @@
+export const planetsData = [
+  {
+    id: 1,
+    name: 'Mercury',
+    diameter: 0.39,
+    distFromSun: 0.39
+  },
+  {
+    id: 2,
+    name: 'Venus',
+    diameter: 0.94,
+    distFromSun: 0.72
+  },
+  {
+    id: 3,
+    name: 'Earth',
+    diameter: 1,
+    distFromSun: 1
+  },
+  {
+    id: 4,
+    name: 'Mars',
+    diameter: 0.53,
+    distFromSun: 1.52
+  },
+  {
+    id: 5,
+    name: 'Jupiter',
+    diameter: 11.2,
+    distFromSun: 5.2
+  },
+  {
+    id: 6,
+    name: 'Saturn',
+    diameter: 9.5,
+    distFromSun: 9.6
+  },
+  {
+    id: 7,
+    name: 'Uranus',
+    diameter: 4,
+    distFromSun: 19.2
+  },
+  {
+    id: 8,
+    name: 'Neptune',
+    diameter: 3.9,
+    distFromSun: 30
+  },
+  {
+    id: 9,
+    name: 'Pluto',
+    diameter: 0.19,
+    distFromSun: 39.5
+  },
+]
