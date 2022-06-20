c7285397428962f9519552c6456eafd36aae1dd7
benji77fr <b.girardpro@gmail.com>
2022-06-20 21:46:24 +0200

    Delete planetData context

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import { PlanetsContextProvider } from './context/PlanetsContext'
 
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <PlanetsContextProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </PlanetsContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
