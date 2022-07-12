a18a91e7e5880d31fb52168008d11d5e277d87ba
benji77fr <b.girardpro@gmail.com>
2022-07-12 17:22:15 +0200

    Change baseURL

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/solar-system',
   plugins: [
     react()
   ],
